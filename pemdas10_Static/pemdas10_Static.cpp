96dd63a3bf66f2a7544c834941c5f5b071512ea3
RMadya <kereyoben75@gmail.com>
2024-05-13 10:29:52 +0700

    identifikasi fungsi dan variabel, public

--- pemdas10_Static/pemdas10_Static.cpp
+++ pemdas10_Static/pemdas10_Static.cpp
@@ -1,20 +1,13 @@
-// pemdas10_Static.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class mahasiswa {
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+public:
+    static int nim;
+    int id;
+    string nama;
+};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
