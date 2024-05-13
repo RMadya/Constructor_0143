ab51efebe5707a6a5940555d469303080659fc55
RMadya <kereyoben75@gmail.com>
2024-05-13 10:55:46 +0700

    deklarasi public, integer, set id dan nim

--- construction2/construction2.cpp
+++ construction2/construction2.cpp
@@ -4,3 +4,16 @@ using namespace std;
 class mahasiswa {
 private:
 	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; }
+	static int getNim() { return nim; }
+
+	mahasiswa(string pnama) : nama(pnama) { setID(); }
+
+};
