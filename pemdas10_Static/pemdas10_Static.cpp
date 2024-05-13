791ea68d17a876698c4fcca4ef18d7f2aa0c2d68
RMadya <kereyoben75@gmail.com>
2024-05-13 10:32:25 +0700

    identifikasi void setid

--- pemdas10_Static/pemdas10_Static.cpp
+++ pemdas10_Static/pemdas10_Static.cpp
@@ -8,6 +8,15 @@ public:
     static int nim;
     int id;
     string nama;
+
+    void setID();
+    void printAll();
+    mahasiswa(string pnama) {
+        nama = pnama;
+        setID();
+    }
 };
+
+
 
 
