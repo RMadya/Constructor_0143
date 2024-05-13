cf9f60c6ab601bc37d5134db174c53fc04719b53
RMadya <kereyoben75@gmail.com>
2024-05-13 18:29:24 +0700

    integer main pemanggilan constructor

--- DesductorPemdas/DesductorPemdas.cpp
+++ DesductorPemdas/DesductorPemdas.cpp
@@ -18,3 +18,31 @@ angka::angka(int i) {//constructor
     arr = new int[i];
     isiData();
 }
+angka::~angka() {//constructor
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData() {
+    for (int i = 1; i <= panjang; i++) {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData() {
+    for (int i = 1; i <= panjang; i++) {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
+int main() {
+    angka belajarcpp(3); //constructor dipanggil
+    angka* ptrBelajarcpp = new angka(5); //constructor dipanggil
+    delete ptrBelajarcpp; //destructor dipanggil
+
+    return 0;
+}//destructor dipanggil
